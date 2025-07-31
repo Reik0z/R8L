029dd3f5a5a49ffb4aa202a51a76556ba42a137e
Kosmonautt <b.contreras.s@pm.me>
2025-07-31 00:12:09 -0400

    feat: added code for testing input

--- assets/PSX_Style_Cars_by_GGBot_(August2023)/Car 07/car_7.gd
+++ assets/PSX_Style_Cars_by_GGBot_(August2023)/Car 07/car_7.gd
@@ -1,7 +1,14 @@
 extends Node3D
 
-@export var angular_speed: int = PI
+@export var angular_speed: int = 1
 
 
 func _process(delta: float) -> void:
 	rotate_y(angular_speed * delta)
+	
+func _input(event):
+	if event.is_action_pressed("ui_left"):
+		angular_speed -=1
+	elif event.is_action_pressed("ui_right"):
+		angular_speed +=1
+	
