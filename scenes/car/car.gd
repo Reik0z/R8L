e1735a1ce28964cdff0d16fb9887fea61a8243c3
Reik0z <demian.hott@ug.uchile.cl>
2025-08-01 00:08:28 -0400

    feat: added derail logic

--- scenes/car/car.gd
+++ scenes/car/car.gd
@@ -1,13 +1,49 @@
 extends PathFollow3D
 
-@export var speed: float = 3.0
+@export var base_speed: float = 3.0
+@export var boost_multiplier: float = 2.0
+@export var derail_threshold: float = 0.7
+@export var derail_speed_reduction: float = 0.9
+
+var current_speed: float = 0.0
+var is_derailed: bool = false
+var is_boosting: bool = false
+
+func _ready() -> void:
+	current_speed = base_speed
 
 func _process(delta: float) -> void:
-	progress += speed * delta
-	
+	progress += current_speed * delta
+
+func _physics_process(delta: float) -> void:
+	check_derail()
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-		speed *= 2
+		current_speed = base_speed * boost_multiplier
+		is_boosting = true
 	if event.is_action_released("ui_accept"):
-		speed /= 2
+		current_speed = base_speed
+		is_boosting = false
+
+func check_derail() -> void:
+	if not is_boosting:
+		is_derailed = false
+		return
+	
+	var path_direction = -transform.basis.z.normalized()
+	var ideal_direction = Vector3.FORWARD
+	var alignment = path_direction.dot(ideal_direction)
+	
+	if alignment < derail_threshold:
+		handle_derail()
+
+func handle_derail() -> void:
+	if not is_derailed:
+		is_derailed = true
+		current_speed *= derail_speed_reduction
+		start_derail_effect()
+		print("¡Descarrile detectado!")
+
+func start_derail_effect() -> void:
+	pass
