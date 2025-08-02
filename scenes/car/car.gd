68ced03e6795a9fa3a73aed7acaa538882f7d764
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-08-02 03:19:11 -0400

    feat: added forces, friction coefficient, mass and acceleration to car

--- scenes/car/car.gd
+++ scenes/car/car.gd
@@ -1,17 +1,24 @@
 extends PathFollow3D
 
-@export_category("Movementd")
-@export var base_speed: float = 3.0
-@export var max_speed: float = 30.0
-@export var acceleration: float = 20.0
+@export_category("Car Properties")
+@export var max_speed: float = 25.0
+@export var engine_force: float = 25.0
+@export var mass: float = 1.0
+@export var gravity: float = 9.8
+@export var friction_coefficient: float = 0.95
 
+var current_speed: float = 0.0
+var current_sum_of_forces: float = 0.0
+var current_normal_force: float = mass * gravity
+var current_friction_force: float = 0.0
+var current_acceleration: float = 0.0
 
 @export_category("Derailment")
 @export var boost_multiplier: float = 2.0
 @export var derail_threshold: float = 0.7
 @export var derail_speed_reduction: float = 0.9
+@export var base_speed: float = 3.0
 
-var current_speed: float = 0.0
 var is_derailed: bool = false
 var is_boosting: bool = false
 
@@ -19,15 +26,33 @@ var is_boosting: bool = false
 	#current_speed = base_speed
 
 func _process(delta: float) -> void:
-	# when the button is held, accelerate
+	# when the button is held, apply engine force
 	if Input.is_action_pressed("ui_accept"):
-		current_speed += acceleration * delta
+		current_sum_of_forces = engine_force - current_friction_force
+	else:
+		current_sum_of_forces = - current_friction_force
+		
+	# if the car is moving, calculate the friction force
+	if current_speed > 0.0:
+		current_friction_force = current_normal_force * friction_coefficient
+	else:
+		current_friction_force = 0.0
 	
+	# the acceleration is calculated
+	current_acceleration = current_sum_of_forces / mass
+	
+	# the speed is calculated
+	current_speed += current_acceleration * delta
 	# speed is clamped
 	current_speed = clampf(current_speed, 0.0, max_speed)
 	
+	print(current_speed)
+	
 	# progress is calculated
 	progress += current_speed * delta
+	
+	#print(current_speed)
+	
 
 #func _physics_process(delta: float) -> void:
 	#check_derail()
