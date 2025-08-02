3f9c5d46ed7b886300c3643b05d41d99315c904f
Kosmonautt <b.contreras.2@ug.uchile.cl>
2025-08-02 04:05:34 -0400

    feat: added gravity force to calculations when moving car though the rail

--- scenes/car/car.gd
+++ scenes/car/car.gd
@@ -5,12 +5,14 @@ extends PathFollow3D
 @export var engine_force: float = 25.0
 @export var mass: float = 1.0
 @export var gravity: float = 9.8
-@export var friction_coefficient: float = 0.95
+@export var friction_coefficient: float = 0.95            
 
 var current_speed: float = 0.0
 var current_sum_of_forces: float = 0.0
 var current_normal_force: float = mass * gravity
 var current_friction_force: float = 0.0
+# gravity force component that acts on rail
+var current_in_rail_gravity_force: float = 0.0
 var current_acceleration: float = 0.0
 
 @export_category("Derailment")
@@ -28,15 +30,21 @@ var is_boosting: bool = false
 func _process(delta: float) -> void:
 	# when the button is held, apply engine force
 	if Input.is_action_pressed("ui_accept"):
-		current_sum_of_forces = engine_force - current_friction_force
+		current_sum_of_forces = engine_force + current_friction_force + current_in_rail_gravity_force
 	else:
-		current_sum_of_forces = - current_friction_force
+		current_sum_of_forces = current_friction_force + current_in_rail_gravity_force
 		
+	## if the car is not moving, there's no friction force
+	#if abs(current_speed - 0.0) < 0.1:
+		#current_friction_force = 0.0
 	# if the car is moving, calculate the friction force
 	if current_speed > 0.0:
+		current_friction_force = - current_normal_force * friction_coefficient
+	elif current_speed < 0.0:
 		current_friction_force = current_normal_force * friction_coefficient
-	else:
-		current_friction_force = 0.0
+	
+	# the gravity force affecting the direction of the car is calculated
+	current_in_rail_gravity_force = - current_normal_force * cos(deg_to_rad(90 - rotation_degrees.x))
 	
 	# the acceleration is calculated
 	current_acceleration = current_sum_of_forces / mass
@@ -44,14 +52,13 @@ func _process(delta: float) -> void:
 	# the speed is calculated
 	current_speed += current_acceleration * delta
 	# speed is clamped
-	current_speed = clampf(current_speed, 0.0, max_speed)
-	
-	print(current_speed)
+	current_speed = clampf(current_speed, - max_speed, max_speed)
 	
 	# progress is calculated
 	progress += current_speed * delta
 	
-	#print(current_speed)
+	# angle with gravity
+	print(current_speed)
 	
 
 #func _physics_process(delta: float) -> void:
